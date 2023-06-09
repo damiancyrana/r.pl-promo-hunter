a4ce673b3e86a4e68b5d6ce2c0f105f949a9be59
Damian Cyrana <damiancyrana@gmail.com>
2023-06-09 21:25:52 +0200

    Bug fixes related to bad link attribution

--- r.py
+++ r.py
@@ -15,56 +15,47 @@ class EndingOffers:
     def __init__(self):
         self.url_for_ending_offers = "https://r.pl/koncoweczka"
 
+
     def scrape_offers(self):
         response = requests.get(self.url_for_ending_offers)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Find all div elements with class "bloczki-lp__content"
-        divs = soup.find_all('div', class_='bloczki-lp__content')
+        # Find all div elements with class "bloczek__wrapper"
+        wrappers = soup.find_all('div', class_='bloczek__wrapper')
 
         # List to store the offers
         offers = []
 
-        # Iterate over each div
-        for div in divs:
-            # Find all header elements within the div
-            header_elements = div.find_all('div', class_='bloczek__naglowek')
-            # Find all link elements within the div
-            link_elements = div.find_all('a')
-            # Find all price elements within the div
-            price_elements = div.find_all('span', class_='bloczek__cena h1')
-            # Find all location elements within the div
-            location_elements = div.find_all('span', class_='bloczek__lokalizacja--text')
+        for wrapper in wrappers:
+            header_element = wrapper.find('p', class_='bloczek__tytul')
+            link_element = wrapper.find('a')
+            price_element = wrapper.find('span', class_='bloczek__cena')
+            location_element = wrapper.find('span', class_='bloczek__lokalizacja--text')
 
-            # Prepare an offer with header, link, price, and location
-            for header, link, price, location in zip(header_elements, link_elements, price_elements, location_elements):
-                # Combine the relative link with the base URL
-                absolute_link = urljoin(self.url, link['href'])
-
+            if header_element and link_element and price_element and location_element:
+                absolute_link = urljoin(self.url_for_ending_offers, link_element['href'])
                 offer = {
-                    'header': header.text.strip(),
-                    'location': location.text.strip(),
-                    'price': price.text.strip(),
+                    'header': header_element.text.strip(),
+                    'location': location_element.text.strip(),
+                    'price': price_element.text.strip(),
                     'link': absolute_link,
                 }
                 offers.append(offer)
 
         return offers
 
+
     def send_email(self, sender, password, recipient):
         offers = self.scrape_offers()
 
-        # Create the message
         message = MIMEMultipart()
         message['From'] = sender
         message['To'] = recipient
 
-        # Add the date to the subject
         current_date = datetime.now().strftime('%Y-%m-%d')
         subject = f"Ending Offers - {current_date}"
         message['Subject'] = Header(subject, 'utf-8')
 
-        # Create the HTML content for the email body
         body = "<h1>Rainbow Ending Offers</h1>"
         for offer in offers:
             body += f"<h2>{offer['header']}</h2>"
@@ -73,10 +64,8 @@ class EndingOffers:
             body += f"<p>Link: <a href='{offer['link']}'>View Offer</a></p>"
             body += "<hr>"
 
-        # Attach the HTML content to the message
         message.attach(MIMEText(body, 'html'))
 
-        # Send the email
         try:
             server = smtplib.SMTP('smtp.gmail.com', 587)
             server.starttls()
@@ -87,7 +76,6 @@ class EndingOffers:
         except Exception as e:
             print(f"Error sending email: {e}")
 
-# Load email credentials from JSON file
 with open('credentials.json') as json_file:
     credentials = json.load(json_file)
 
